fad551ea5414c34e8d4904c881e77ebc12bd819c
Dylan Penney <penneydylan@icloud.com>
2025-02-12 16:29:23 +0000

    added felx-grow: 1 to main componnent to fill up empty space -> will facilitate the modal relocation plans

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,9 +2,9 @@ import Navbar from "../Navbar/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-background-secondary">
+    <div className="min-h-screen bg-background-secondary flex flex-col items-center">
       <Navbar />
-      <main className="container mx-auto px-4 py-8">{children}</main>
+      <main className="container mx-auto px-4 py-8 flex-1">{children}</main>
     </div>
   );
 }
