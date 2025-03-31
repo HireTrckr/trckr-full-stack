6c82cab543a1c6a7333372705af95a117c3e6164
Dylan Penney <penneydylan@icloud.com>
2025-03-31 17:04:09 +0100

    remove debug console log

--- context/settingStore.ts
+++ context/settingStore.ts
@@ -64,7 +64,6 @@ export const useSettingsStore = create<SettingsStore>((set, get) => ({
         `${settings.theme.primaryColor}`
       );
 
-      console.log(document.documentElement.style);
 
       set({ settings });
     } catch (error) {
