f3d56810b18d7fccf5950bc58d1dea45d8ba20fd
Dylan Penney <penneydylan@icloud.com>
2025-04-12 11:15:47 +0100

    feat: create util function that can generate status skeletons

---
+++ utils/generateSkeletonData.tsx
@@ -0,0 +1,15 @@
+import React, { JSX } from 'react';
+
+export const getSkeletonStatusCards = (count: number): JSX.Element[] => {
+  return [...Array(count)].map((_, index) => (
+    <div
+      key={index}
+      className="flex justify-evenly items-center gap-2 w-full p-2 min-w-full max-w-full border-2 border-gray-700 rounded-lg"
+    >
+      <div className="rounded-full aspect-square h-[1rem] bg-gray-700 animate-pulse" />
+      <div className="flex-1 overflow-x-auto">
+        <div className="h-[1rem] w-32 bg-gray-700 rounded animate-pulse"></div>
+      </div>
+    </div>
+  ));
+};
