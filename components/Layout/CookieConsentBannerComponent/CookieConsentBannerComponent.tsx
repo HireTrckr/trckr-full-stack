4825d56ff99edbf61f64b7e393fde43656b731cb
HireTrckr CI Autoformatter [prettier-bot] <github-actions[bot]@users.noreply.github.com>
2025-05-02 12:18:11 +0000

    Automated changes by prettier-bot

--- components/Layout/CookieConsentBannerComponent/CookieConsentBannerComponent.tsx
+++ components/Layout/CookieConsentBannerComponent/CookieConsentBannerComponent.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 export function CookieConsentBannerComponent(): JSX.Element {
   const { t } = useTranslation();
-  
+
   return (
     <CookieConsent
       location="bottom"
@@ -15,7 +15,10 @@ export function CookieConsentBannerComponent(): JSX.Element {
       }}
     >
       <span className="text-text-primary transition-all duration text">
-        {t('cookies.message', 'This website uses cookies to ensure you are kept logged in.')}
+        {t(
+          'cookies.message',
+          'This website uses cookies to ensure you are kept logged in.'
+        )}
       </span>
     </CookieConsent>
   );
