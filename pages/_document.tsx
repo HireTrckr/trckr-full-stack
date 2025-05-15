550fb215d3e1978bca3181fc6f8dc646c8689205
Dylan Penney <penneydylan@icloud.com>
2025-05-15 11:02:11 +0100

    feat: add vercel speed insights

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 // pages/_document.tsx
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 class MyDocument extends Document {
   render() {
@@ -74,6 +75,7 @@ class MyDocument extends Document {
         <body className="bg-background-secondary">
           <Main />
           <NextScript />
+          <SpeedInsights />
         </body>
       </Html>
     );
